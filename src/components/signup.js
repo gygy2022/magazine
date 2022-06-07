8b1d1f41b1df0f70aa0eed8e65fa0a33c11f6f70
gygy2022 <2022gygy@gmail.com>
2022-06-07 11:39:57 +0900

    회원가입 구조 작업

--- src/components/signup.js
+++ src/components/signup.js
@@ -4,6 +4,26 @@ const SignUp = () => {
   return (
     <>
     <h1>회원가입페이지</h1>
+    <form>
+      <label>아이디
+      <input type="email"/>
+      </label>
+      
+      <label>비밀번호
+      <input type="password"/>
+      </label>
+      
+      <label>비밀번호 확인
+      <input type="text"/>
+      </label>
+      
+      <label>닉네임
+      <input type="text"/>
+      </label>
+      
+      <button>회원가입</button>
+      
+    </form>
     </>
   )
 }
