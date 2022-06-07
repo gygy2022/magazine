75acb421f1333125dc0ba1ad1decc824c0112ead
gygy2022 <2022gygy@gmail.com>
2022-06-07 16:31:15 +0900

    회원가입 기능 기초 작업

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,29 +1,60 @@
-import react from "react";
+import React from "react";
+import {auth, db} from '.././shared/firebase';
+import {createUserWithEmailAndPassword} from 'firebase/auth';
+import { collection, addDoc } from 'firebase/firestore'
+import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+
+  const navigate = useNavigate();
+
+  const id_ref = React.useRef(null);
+  const pw_ref = React.useRef(null);
+  const nickname_ref = React.useRef(null);
+
+const signupFB = async () => {
+  const user = await createUserWithEmailAndPassword(
+    auth,
+    id_ref.current.value,
+    pw_ref.current.value,
+  );
+  console.log(user);
+
+  const user_doc = await addDoc(collection(db,"users"),{
+    user_id : user.user.email,
+    name: nickname_ref.current.value,
+  })
+
+  alert("회원가입을 축하드려요!");
+  navigate("/");
+
+};
+
+
+
   return (
     <>
     <h1>회원가입페이지</h1>
-    <form>
-      <label>아이디
-      <input type="email"/>
+    <div>
+      <label>아이디(이메일)
+      <input type="email" ref={id_ref}/>
       </label>
-      
+
       <label>비밀번호
-      <input type="password"/>
+      <input type="password" ref={pw_ref}/>
       </label>
       
       <label>비밀번호 확인
-      <input type="text"/>
+      <input type="password" />
       </label>
       
       <label>닉네임
-      <input type="text"/>
+      <input type="text" ref={nickname_ref}/>
       </label>
       
-      <button>회원가입</button>
+      <button onClick={signupFB}>회원가입</button>
       
-    </form>
+    </div>
     </>
   )
 }
