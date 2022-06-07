d3621bfe927878756591deac5727262269c1b3bf
gygy2022 <2022gygy@gmail.com>
2022-06-07 11:39:43 +0900

    버튼을 누르면 페이지 이동

--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,13 @@
 import react from "react";
+import { useNavigate } from "react-router-dom";
 
 const Main = (props) => {
+  const navigate = useNavigate();
   return (
     <>
     <h1>메인페이지</h1>
-    <button>로그인</button>
-    <button>회원가입</button>
+    <button onClick={() => {navigate("/login")}}>로그인</button>
+    <button onClick={() => {navigate("/signup")}}>회원가입</button>
     <div>게시물 리스트</div>
     </>
   )
