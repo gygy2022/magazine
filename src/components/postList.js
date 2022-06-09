8db4b91900209f57f5b56d4deb861f214d851d86
gygy2022 <2022gygy@gmail.com>
2022-06-09 20:42:41 +0900

    postList 컴포넌트 style 작업

--- src/components/postList.js
+++ src/components/postList.js
@@ -1,5 +1,6 @@
 import react from "react";
 import { useDispatch, useSelector } from "react-redux";
+import styled  from "styled-components";
 
 
 
@@ -17,16 +18,15 @@ const PostList = () => {
 
   {post_list.map((post, index) => {
         return (
-          <div className="post_card">
-          <h4>-닉네임-</h4>
-          <span className="post_text" id="post_name">{post_list[index].name}</span>
-          <h4>-시간-</h4>
-          <span className="post_text" id="post_time">{post_list[index].time}</span>
-          <h4>-사진-</h4>
-          <img src={post_list[index].image_url} />
-          <h4>-글-</h4>
-          <span className="post_text" id="post_text">{post_list[index].text}</span>
-        </div>
+          <Post_card>
+            <Name_time>
+            <span className="post_text" id="post_name">{post_list[index].name}</span>
+            <span className="post_text" id="post_time">{post_list[index].time}</span>
+            </Name_time>
+          <img src={post_list[index].image_url} id="post_img"/>
+          
+          <p className="post_text" id="post_text">{post_list[index].text}</p>
+        </Post_card>
 
         )
       })}
@@ -36,5 +36,31 @@ const PostList = () => {
   )
 
 };
+
+let Post_card = styled.div`
+border:1px solid goldenrod;
+margin-bottom:20px;
+
+#post_img {
+  width:90%;
+}
+`
+
+let Name_time = styled.div`
+margin: 0 auto;
+width:90%;
+height:50px;
+line-height:50px;
+
+  #post_name {
+    float:left;
+    font-size:14px;
+  }
+
+  #post_time {
+    float:right;
+    font-size:12px;
+  }
+`
 
 export default PostList;
